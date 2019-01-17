ce03d1fd8b7bfb1869eaede626a929ab6edd1f4e
Alireza Saremi <bestrah.saremi@gmail.com>
2019-01-17 12:35:22 +0330

    ready to upload

--- myshop/settings/production.py
+++ myshop/settings/production.py
@@ -1,6 +1,11 @@
-
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS =  ['https://evening-journey-43840.herokuapp.com/']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 STATIC_URL = '/static/'
